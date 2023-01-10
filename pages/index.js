9970363fef66c241c04c01683921d00611e6ead2
Neeraj Sachdeva <neeraj.sachdeva@nagarro.com>
2023-01-09 21:46:24 -0500

    CSS

--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,22 @@ export default function Home() {
         <h1>Enjoy Games!</h1>
 
         <table>
-          <tr><td>Game1: </td><td>Catch Pi if you can</td><td><a href="https://neerajsachdeva.github.io/pi-game-wc/" target="iFrameGame"> ==>Play</a></td></tr>
-          <tr><td>Game2 </td><td>Snakes & Ladders</td><td> <a href="https://neerajsachdeva.github.io/snake-ladder-game-wc/" target="iFrameGame"> ==>Play</a> </td></tr>
-
-
-          <iframe name="iFrameGame" src=""></iframe>
+          <tr>
+			  <td>Game 1: </td>
+			  <td><b>Catch Pi if you can</b></td>
+			  <td><a href="https://neerajsachdeva.github.io/pi-game-wc/" target="iFrameGame">Try Me!</a></td>
+		 </tr>
+          <tr>
+			  <td>Game 2: </td>
+			  <td><b>Snakes & Ladders</b></td>
+			  <td> <a href="https://neerajsachdeva.github.io/snake-ladder-game-wc/" target="iFrameGame">Try Me!</a></td>
+		  </tr>
+       
         </table>
-
+        <iframe name="iFrameGame" class="gameFrame" src=""></iframe>
       </main>
 
-      <Footer />
+   
     </div>
   )
 }
