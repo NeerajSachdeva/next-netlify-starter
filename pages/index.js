728564ae1af0545be38d2dccc86a982afdac8d97
Neeraj Sachdeva <neeraj.sachdeva@nagarro.com>
2023-01-09 20:34:41 -0500

    updated html

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,13 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1>Enjoy Games!</h1>
+
+		<table>
+			<tr><td>Game1: </td><td>Catch the PI</td><td><button type="button" onclick="window.location.href='https://neerajsachdeva.github.io/pi-game-wc/';">Play</button></td></tr>
+			<tr><td>Game2: </td><td>Snakes & Ladders</td><td><button type="button" onclick="window.location.href='https://neerajsachdeva.github.io/snake-ladder-game-wc/';">Play</button></td></tr>
+		</table>
+
       </main>
 
       <Footer />
